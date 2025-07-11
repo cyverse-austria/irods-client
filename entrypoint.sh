55a020edca95832d04660896c30e7392b48631b3
Mojib Wali <mb_wali@hotmail.com>
2025-07-11 09:59:38 +0200

    irods env: add default port

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@
 cat > /root/.irods/irods_environment.json <<EOF
 {
   "irods_host": "${IRODS_HOST}",
+  "irods_port": 1247,
   "irods_user_name": "${IRODS_USER_NAME}",
   "irods_zone_name": "${IRODS_ZONE_NAME}"
 }
